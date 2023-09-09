70ccbed3e619cec1aa368f7308fac263db875532
leily <leily@example.com>
2023-09-09 15:09:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main (String[] args) {
+        int amount;
+        amount = 1100;
+        int chet;
+        chet = 200;
+        int percent;
+        if (amount > 1000) {
+            percent = 10;
+        } else {
+            percent = 0;
+        }
+        System.out.println(chet + amount + (amount / 100));}
+
+    }
+
