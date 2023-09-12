b22dcf76292ca754421ecd151c8820fc9c349dd8
leily <leily@example.com>
2023-09-12 22:11:23 +0300

    Removed two lines into one

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int amount;
-        amount = 1100;
-        int chet;
-        chet = 200;
+        int amount = 1100;
+        int balance = 200;
         int percent;
         if (amount > 1000) {
             percent = amount / 100;
         } else {
             percent = 0;
         }
-        System.out.println(chet + amount + (amount / 100));
+        System.out.println(balance + amount + (amount / 100));
         System.out.println(amount / 100);
     }
 
